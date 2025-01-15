4a6aad52a3718f3c40a0b9991981604ece2e4faa
upeka-silva <upekasilva2131@gmail.com>
2025-01-15 23:06:52 +0530

    click event class changed

--- linklytics-URL-shortening/src/main/java/com/url/linklytics_/shortening/model/ClickEvent.java
+++ linklytics-URL-shortening/src/main/java/com/url/linklytics_/shortening/model/ClickEvent.java
@@ -1,10 +1,8 @@
 package com.url.linklytics_.shortening.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.time.LocalDateTime;
-@Data
 @Entity
 @Table(name = "click_events")
 public class ClickEvent {
@@ -15,4 +13,39 @@ public class ClickEvent {
     @ManyToOne
     @JoinColumn(name = "url_mapping_id")
     private UrlMapping urlMapping;
+
+
+    public ClickEvent() {
+    }
+
+    public ClickEvent(Long id, LocalDateTime createdDate, UrlMapping urlMapping) {
+        this.id = id;
+        this.createdDate = createdDate;
+        this.urlMapping = urlMapping;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public UrlMapping getUrlMapping() {
+        return urlMapping;
+    }
+
+    public void setUrlMapping(UrlMapping urlMapping) {
+        this.urlMapping = urlMapping;
+    }
 }
